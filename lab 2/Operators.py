1b0258005a541fe4b22aea13f37bc49e3c8b39ca
jasikr <102432301+jasikr@users.noreply.github.com>
2024-01-30 14:45:22 +0600

    Add files via upload

---
+++ lab 2/Operators.py
@@ -0,0 +1,49 @@
+#ex1
+x = "Hello World"
+print(len(x))
+
+#ex2
+txt = "Hello World"
+x = txt[0]
+
+#ex3
+txt = "Hello World"
+x = txt[2:5]
+
+#ex4
+txt = " Hello World "
+x = txt.strip()
+
+#ex5
+txt = "Hello World"
+txt = txt.upper()
+
+#ex6
+txt = "Hello World"
+txt = txt.lower()
+
+#ex7
+txt = "Hello World"
+txt = txt.replace("H","J")
+
+#ex8
+age = 36
+txt = "My name is John, and I am {}"
+#ex1
+print(10 * 5)
+
+#ex2
+print(10/2)
+
+#ex3
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+#ex4
+if 5 !=10:
+  print("5 and 10 is not equal")
+
+#ex5
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
